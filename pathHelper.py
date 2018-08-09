ab06730844a771813044f0656475d473a8010d57
tsny <tsny700@gmail.com>
2018-08-08 21:53:53 -0500

    New file structure, file validation

---
+++ pathHelper.py
@@ -0,0 +1,10 @@
+from os import path
+
+def ValidateFiles():
+    if not (path.isfile('login.json')):
+        raise FileNotFoundError('Could not find login details')
+
+    if not (path.exists('./memes')):
+        raise FileNotFoundError('Could not find memes folder')
+
+ValidateFiles()
