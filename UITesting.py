1f260720831f4bd11090964798f05a38cf404891
tsny <tsny700@gmail.com>
2018-08-11 14:42:26 -0500

    Labels and entries. Labels have set widths to hold long strings.

--- UITesting.py
+++ UITesting.py
@@ -6,33 +6,39 @@ class Application(tk.Frame):
         self.pack()
         self.createWidgets()
 
-    def testFunc(self):
-        print("test")
+    def Update(self):
+        print("Update TODO")
 
     def createWidgets(self):
-        self.consumerKeyEntry = tk.Entry()
+        self.consumerKeyLabel = tk.Label(text="Consumer Key")
+        self.consumerKeyLabel.pack()
+        self.consumerKeyEntry = tk.Entry(width=200)
         self.consumerKeyEntry.pack()
 
-        self.consumerSecretEntry = tk.Entry()
+        self.consumerSecretLabel = tk.Label(text="Consumer Secret")
+        self.consumerSecretLabel.pack()
+        self.consumerSecretEntry = tk.Entry(width=200)
         self.consumerSecretEntry.pack()
 
-        self.accessKeyEntry = tk.Entry()
+        self.accessKeyLabel = tk.Label(text="Access Key")
+        self.accessKeyLabel.pack()
+        self.accessKeyEntry = tk.Entry(width=200)
         self.accessKeyEntry.pack()
 
-        self.accessSecretEntry = tk.Entry()
+        self.accessSecretLabel = tk.Label(text="Access Secret")
+        self.accessSecretLabel.pack()
+        self.accessSecretEntry = tk.Entry(width=200)
         self.accessSecretEntry.pack()
 
-        self.QUIT = tk.Button(self, text="QUIT", fg="red",
-                                            command=root.destroy)
-
+        self.QUIT = tk.Button(self, text="QUIT", fg="red", command=root.destroy)
         self.QUIT.pack(side="bottom")
 
-        self.update = tk.Button(self, text="Update Settings", command=self.testFunc)
+        self.update = tk.Button(self, text="Update Settings", command=self.Update)
         self.update.pack()
 
 
 root = tk.Tk()
-root.geometry("300x300")
+root.geometry("600x300")
 root.title("Settings")
 root.resizable(False, False)
 
